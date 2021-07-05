9559418c6b3213c7ec410c857063ac6ca70572fb
svetaspeed <svetaspeedster@gmail.com>
2021-07-05 19:17:41 +1000

    commit 2

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from mainapp.models import *
+
+
+admin.site.register(Cart)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(CartProduct)
+admin.site.register(Customer)
