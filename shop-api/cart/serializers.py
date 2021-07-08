dc8f10ba1de6ae4010a44f550459b92046ab0078
svetaspeed <svetaspeedster@gmail.com>
2021-07-08 13:42:41 +1000

    commit 3

--- shop-api/cart/serializers.py
+++ shop-api/cart/serializers.py
@@ -1,6 +1,39 @@
 from rest_framework import serializers
 
-from mainapp.models import Cart, Customer
+from mainapp.models import Cart, Customer, CartProduct
+from ..main.serializers import ProductSerializer
 
 
 
+class CartProductSerializer(serializers.ModelSerializer):
+
+    product = ProductSerializer
+
+    class Meta:
+        model = CartProduct
+        fields = ['id', 'product', 'qty', 'final_price']
+
+class CustomerSerializer(serializers.ModelSerializer):
+
+    user = serializers.SerializerMethodField
+
+    class Meta:
+        model = Customer
+        fields = '__all__'
+
+    @staticmethod
+    def get_user(obj):
+        first_name, last_name = obj.user.first_name, obj.user.last_name
+        if not (first_name and last_name):
+            return obj.username
+        return ' '.join([first_name, last_name])
+
+
+class CartSerializer(serializers.ModelSerializer):
+
+    products = CartProductSerializer(many=True)
+    owner = CustomerSerializer
+
+    class Meta:
+        model = Cart
+        fields = '__all__'
--- shop-api/main/serializers.py
+++ shop-api/main/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from mainapp.models import Product, Cart, Customer, CartProduct, Category
+from mainapp.models import Product, Cart, Customer, Category
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -17,3 +17,4 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
