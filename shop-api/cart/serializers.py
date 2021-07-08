2946b285d3cd2d67a1eb86604b2f3ada1718a457
svetaspeed <svetaspeedster@gmail.com>
2021-07-08 13:11:47 +1000

    commit 3

---
+++ shop-api/cart/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+
+from mainapp.models import Cart, Customer
+
+
+
---
+++ shop-api/main/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+
+from mainapp.models import Product, Cart, Customer, CartProduct, Category
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class ProductSerializer(serializers.ModelSerializer):
+
+    category = CategorySerializer
+
+    class Meta:
+        model = Product
+        fields = '__all__'
